5fdedf662801ae08419dc74faf6bdb4726c77d70
Nakanishi, Yusuke <super.yusuke0000@gmail.com>
2018-11-17 16:27:52 +0900

    最初のコミット

---
+++ components/Sample.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <button
+    type="button"
+    name="button"
+    @click="plus">click times: {{ num }}
+  </button>
+</template>
+​
+<script>
+  export default {
+    data() {
+      return {
+        name: 'takanorip',
+        num: 0
+      }
+    },
+    methods: {
+      plus() {
+        this.num++
+      }
+    }
+  }
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+  <nuxt-link to="/">TOP</nuxt-link>
+</template>
+​
+<script>
+  export default {}
+</script>
+​
+<style lang="scss">
+</style>
